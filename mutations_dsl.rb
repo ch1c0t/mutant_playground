95ca42d3ff19458b5752e5c2ae5c5fd55612038f
Anatoly Chernow <chertoly@gmail.com>
2017-04-06 23:21:55 +0300

    Add mutations_dsl.rb

---
+++ mutations_dsl.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+require 'adamantium'
+require 'anima'
+require 'mutant/ast'
+require 'mutant/ast/sexp'
+require 'concord'
+require 'unparser'
+require 'mutant/ast/types'
+require 'mutant/meta'
+
+binding.pry
+Mutant::Meta::Example::All
